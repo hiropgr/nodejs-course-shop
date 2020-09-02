c8fb33fba3a52f5ebb1d992b8fe65a5df78b6feb
Reyhaz <karasta86@gmail.com>
2020-09-02 18:15:59 +0500

    feat: keys protection

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
-module.exports = {
-    'MONGODB_URI': 'mongodb+srv://hiropgr:TYV4EFGWL2ZHeb7C@cluster0.r71tx.mongodb.net/courses?retryWrites=true&w=majority',
-    'SESSION_SECRET': 'some secret value'
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod');
+} else {
+    module.exports = require('./keys.dev');
 }
---
+++ keys/keys.dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'MONGODB_URI': 'mongodb+srv://hiropgr:TYV4EFGWL2ZHeb7C@cluster0.r71tx.mongodb.net/courses?retryWrites=true&w=majority',
+    'SESSION_SECRET': 'some secret value',
+}
