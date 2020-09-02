30b3d4f374872081c74546e40f2fa67a7ccd9153
Reyhaz <karasta86@gmail.com>
2020-09-02 19:34:15 +0500

    make it better

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,4 +1,4 @@
 module.exports = {
     'MONGODB_URI': 'mongodb+srv://hiropgr:TYV4EFGWL2ZHeb7C@cluster0.r71tx.mongodb.net/courses?retryWrites=true&w=majority',
-    'SESSION_SECRET': 'some secret value',
+    'SESSION_SECRET': 'some secret',
 }
